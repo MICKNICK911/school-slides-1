3d8b8e0720db34b33743ea257dfe845d89cc8d77
micknick911 <appiahm29@yahoo.com>
2026-07-31 11:37:04 +0000

    all

---
+++ a-pub/lessons_data.js
@@ -0,0 +1,32 @@
+window.LESSONS = [
+  {
+    lessonNumber: 1, pageNumber: 1, letterTile: "Aa", imageCaption: "Apple",
+    words: [["cat", "bat", "hat", "mat"], ["dad", "fat", "sat", "rat"], ["jam", "fan", "can", "car"]],
+    sentences: ["The cat sat on a mat.", "A rat had a hat.", "The cat is mad.", "The rat ran to a van.", "The cat had a nap."],
+    fillBlanks: ["c__t", "b__t", "h__t", "m__t", "d__d", "f__t", "s__t", "r__t"],
+  },
+  {
+    lessonNumber: 2, pageNumber: 2, letterTile: "Ee", imageCaption: "Net",
+    words: [["net", "bed", "hen", "men"], ["let", "fed", "set", "red"], ["jet", "get", "ten", "vet"]],
+    sentences: ["Jen has a pet hen.", "The hen is wet.", "Jen gets a red net.", "The hen sits on a bed.", "Ten men let the hen rest."],
+    fillBlanks: ["n__t", "b__d", "h__n", "m__n", "l__t", "g__t", "t__n", "r__d"],
+  },
+  {
+    lessonNumber: 3, pageNumber: 3, letterTile: "Ii", imageCaption: "Pin",
+    words: [["sit", "hit", "fit", "tin"], ["mix", "lip", "pin", "fix"], ["pit", "win", "sip", "pit"]],
+    sentences: ["Tim has a big pig.", "The pig can sit.", "Tim hits the lid.", "The pig digs in the bin.", "Tim wins a pin."],
+    fillBlanks: ["s__t", "h__t", "f__t", "t__n", "p__g", "p__t", "s__p", "w__n"],
+  },
+  {
+    lessonNumber: 4, pageNumber: 4, letterTile: "Oo", imageCaption: "Mop",
+    words: [["hot", "lot", "fox", "top"], ["not", "rot", "got", "pot"], ["bot", "cot", "dot", "mob"]],
+    sentences: ["Bob has a red fox.", "The fox hops on a log.", "Bob gets a mop and a pot.", "The fox stops and drops the top.", "Bob hops with the fox."],
+    fillBlanks: ["p__t", "B__b", "f__x", "t__p", "l__t", "g__t", "m__p", "h__p"],
+  },
+  {
+    lessonNumber: 5, pageNumber: 5, letterTile: "Uu", imageCaption: "Bug",
+    words: [["sun", "bug", "run", "hug"], ["gun", "bus", "gum", "rug"], ["fun", "cut", "but", "mud"]],
+    sentences: ["Bud has a bug.", "The bug sits on a mug.", "Bud runs to the sun.", "The bug is on the rug.", "Bud hugs the bug."],
+    fillBlanks: ["b__t", "b__d", "h__g", "r__g", "s__n", "b__s", "m__d", "g__n"],
+  },
+];
